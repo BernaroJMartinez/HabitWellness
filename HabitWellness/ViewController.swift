fa50d27d081374e80e02da10e4ad2885ab738a68
Bernardo Martinez <bjm72@duke.edu>
2020-07-27 15:48:45 -0400

    Initial Commit

---
+++ HabitWellness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HabitWellness
+//
+//  Created by Bernardo Martinez on 7/27/20.
+//  Copyright © 2020 Bernardo Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
